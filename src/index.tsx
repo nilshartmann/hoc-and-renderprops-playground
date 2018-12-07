22170e45302e51073182a728a54d55fa6a158f9b
Nils Hartmann <nils@nilshartmann.net>
2018-12-07 07:48:17 +0100

    remove unused code

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+
+import Greetings from "./01_plain/Greetings";
+// import Greetings from "./02_hoc/Greetings";
+// import Greetings from "./03_renderprops/Greetings";
+// import Greetings from "./04_renderpropsAsChildren/Greetings";
+// import Greetings from "./05_renderpropsWithErrorHandling/Greetings";
+
+class App extends React.Component {
+  render() {
+    return <Greetings title="Greetings" />;
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
